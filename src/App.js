139fe9296eb44249b8a15b5c7eeb9cc1feca02fe
erosmartz <107828862+erosmartz@users.noreply.github.com>
2023-03-20 23:47:37 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React from 'react'
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline'
+
+
+/* COMPONENTS */
+import Navbar from "./components/NavBar/NavBar"
+
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 const App = () => {
   return (
-    <div>App</div>
+    
+    <ThemeProvider theme={darkTheme}>
+    <React.Fragment>
+      
+      <CssBaseline />
+      
+      {
+        
+          <Navbar /> 
+          <ItemListContainer name="Doom Eternal" desc="Un FPS lanzado en 2020" img="https://thegatorseye.com/wp-content/uploads/2022/12/Doom-Eternal-900x900.png" /> 
+          
+        
+      }
+    </React.Fragment>
+    </ThemeProvider>
+
+    
+    
   )
 }
 
---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CartItem = () => {
+  return (
+    <div>cartitem</div>
+  )
+}
+
+export default CartItem
