d0e5664ad7daa09d13c755e540e0d417619f1276
erosmartz <107828862+erosmartz@users.noreply.github.com>
2023-04-04 01:53:39 -0300

    Update ItemListContainer.js

--- src/pages/Home/ItemListContainer.js
+++ src/pages/Home/ItemListContainer.js
@@ -3,7 +3,6 @@ import ProductCard from "../../components/ProductCard/ProductCard"
 import Container from '@mui/material/Container';
 import "./styles.css"
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
 
